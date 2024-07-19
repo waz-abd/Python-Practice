25342ded4316393cffdd31b43c30cc4ac3693efc
wasayabdul <wasay54@hotmail.com>
2024-07-18 23:15:30 -0400

    Added Old Python Questions

---
+++ practice.py
@@ -0,0 +1,36 @@
+# Previous Questions from Old Files
+'''Q1
+name = input("What is your name? ")
+age = input("What is your age? ")
+future_age = (2024 - int(age)) + 100
+print("Hi " + str(name) + ", you will be 100 years old in the year " + str(future_age) + "!")
+'''
+
+'''Q2
+number = input("Hi, please enter a number: ")
+remainder = int(number) % 2
+if remainder == 0:
+    print("Your number is an even number!")
+else:
+    print("Your number is an odd number!")
+'''
+
+'''Q33
+print("Hi, we know the birthdays of Ronaldo, Messi, and Neymar! ")
+birthdays = {'Ronaldo':'February 5, 1985', 'Messi':'June 24, 1987', 'Neymar':'February 5, 1992'}
+choice_select = input("Whose birthday do you want to look up? ")
+print(birthdays[choice_select])
+'''
+
+'''Q28
+def maximum (x,y,z):
+    if x > y > z:
+        return(x)
+    if y > x > z:
+        return(y)
+    if z > y > x:
+        return(z)
+
+numbers = maximum(4,6,3)
+print(numbers)
+'''
