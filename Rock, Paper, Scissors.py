d22360f2752210ee460129626be0f695a95c5e32
wasayabdul <wasay54@hotmail.com>
2024-07-22 23:42:34 -0400

    working on rps

--- Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors.py
@@ -1,3 +1,16 @@
-a = [1, 1, 2, 2, 3, 3, 5, 8, 13, 21, 34, 55, 89]
-for element in a:
-    
+# This is a responsive Rock, Paper, and Scissors Game!
+# Enjoy! :)
+
+print("Welcome to the rock, paper, and scissors game!")
+p1 = input("What is the first player name? ")
+p2 = input("What is the second player name? ")
+print("Welcome " + p1 + " and " + p2 + " !")
+
+choice_1 = input(p1 + " goes first! What do you select? Rock, Paper, or Scissors? ")
+choice_2 = input(p1 + " goes first! What do you select? Rock, Paper, or Scissors? ")
+rock = 1
+scissor = 0
+paper = 1
+
+if choice_1.upper() == "rock" & choice_2.upper() == "paper":
+    print("hi")
